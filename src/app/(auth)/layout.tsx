a866da3b07d491b5175518b7cc3fd86322d9226d
djh592 <dingjh21@outlook.com>
2024-10-13 03:35:51 +0800

    feat: add home page

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,23 +1,18 @@
 import Image from 'next/image';
 import Link from 'next/link';
-import { AppBar, Box } from '@mui/material';
+import { AppBar, Box, Toolbar } from '@mui/material';
 
 export default function AuthLayout({ children }: { children: React.ReactNode }) {
     return (
         <>
-            <AppBar
-                position="static"
-                color="default"
-                sx={{
-                    display: 'flex',
-                    boxShadow: 'none',
-                }}
-            >
-                <Link href="/">
-                    <Box sx={{ pl: 2, pt: 1, }}>
-                        <Image src="/nexio_trans_bg.png" alt="Nexio" width={125} height={50} />
-                    </Box>
-                </Link>
+            <AppBar position="static" color="default" sx={{ boxShadow: 'none' }}>
+                <Toolbar>
+                    <Link href="/">
+                        <Box sx={{ flexGrow: 1 }}>
+                            <Image src="/nexio_trans_bg.png" alt="Nexio" width={125} height={50} />
+                        </Box>
+                    </Link>
+                </Toolbar>
             </AppBar>
             <Box
                 sx={{
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,52 @@
-import { Button } from "@mui/material";
+import React from 'react';
+import { Box, Button, Typography, Container, AppBar, Toolbar } from '@mui/material';
+import Link from 'next/link';
+import Image from 'next/image';
 
-export default function Home() {
+export default function HomePage() {
   return (
-    <div >
-      <Button variant="contained" color="primary">
-        Welcome to Nexio
-      </Button>
-    </div>
+    <>
+      <AppBar position="static" color="default" sx={{ boxShadow: 'none' }}>
+        <Toolbar>
+          <Link href="/">
+            <Box sx={{ flexGrow: 1 }}>
+              <Image src="/nexio_trans_bg.png" alt="Nexio" width={125} height={50} />
+            </Box>
+          </Link>
+        </Toolbar>
+      </AppBar>
+      <Container
+        maxWidth="md"
+        sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          justifyContent: 'center',
+          height: '100%',
+          mt: 8,
+          textAlign: 'center',
+        }}
+      >
+        <Image src="/nexio_trans_bg.png" alt="Nexio" width={250} height={100} />
+        <Typography variant="h2" sx={{ mt: 2, mb: 4 }}>
+          Connect, Engage, Thrive
+        </Typography>
+        <Typography variant="h6" sx={{ mb: 4 }}>
+          Welcome to Nexio, the platform that connects you with the world.
+        </Typography>
+        <Box>
+          <Link href="/signin" passHref>
+            <Button color="primary" variant="outlined" sx={{ mr: 2 }}>
+              Sign In
+            </Button>
+          </Link>
+          <Link href="/signup" passHref>
+            <Button color="primary" variant="contained">
+              Sign Up
+            </Button>
+          </Link>
+        </Box>
+      </Container>
+    </>
   );
 }
