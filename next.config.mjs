3bcef6c8a795036c55c3d0fca1477c3f2e74395c
djh592 <dingjh21@outlook.com>
2024-10-13 18:45:18 +0800

    fix: modify next.config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    output: "standalone",
+    reactStrictMode: false, /* @note: To prevent duplicated call of useEffect */
+    swcMinify: true,
+
+    async rewrites() {
+        return [{
+            source: "/api/:path*",
+            // TODO: Change the destination to the actual API URL
+            destination: "http://127.0.0.1:8000/:path*",
+        }];
+    }
+};
 
 export default nextConfig;
