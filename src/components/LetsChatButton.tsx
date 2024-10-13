6e85ea58f45a63e28f4432be53de579ca0321d5e
djh592 <dingjh21@outlook.com>
2024-10-13 15:43:06 +0800

    fix: beautify LetsChatButton

--- src/components/LetsChatButton.tsx
+++ src/components/LetsChatButton.tsx
@@ -3,26 +3,42 @@ import React from 'react';
 import { Button } from '@mui/material';
 import { useRouter } from 'next/navigation';
 import { useAppSelector } from '@/lib/hooks';
+import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
 
 export default function LetsChatButton() {
-  const router = useRouter();
-  const user = useAppSelector((state) => state.auth.user);
+    const router = useRouter();
+    const user = useAppSelector((state) => state.auth.user);
 
-  const handleClick = () => {
-    if (user && user.id) {
-      router.push('/chat');
-    } else {
-      router.push('/signin');
-    }
-  };
+    const handleClick = () => {
+        if (user && user.id) {
+            router.push('/chat');
+        } else {
+            router.push('/signin');
+        }
+    };
 
-  return (
-    <Button
-      color="primary"
-      variant="contained"
-      onClick={handleClick}
-    >
-      Let&apos;s Chat
-    </Button>
-  );
+    return (
+        <Button
+            color="primary"
+            sx={{
+                mt: 1,
+                height: '50px',
+                width: '200px',
+                backgroundColor: '#1976d2',
+                color: '#fff',
+                '&:hover': {
+                    backgroundColor: '#1565c0',
+                },
+                boxShadow: '0 3px 5px 2px rgba(33, 203, 243, .3)',
+                textTransform: 'none',
+                fontSize: '16px',
+                fontWeight: 'bold',
+            }}
+            variant="contained"
+            endIcon={<ArrowForwardIcon />}
+            onClick={handleClick}
+        >
+            Let&apos;s Chat
+        </Button>
+    );
 }
