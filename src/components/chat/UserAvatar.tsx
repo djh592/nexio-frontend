80b3e306705d4947b292cd5179d38d1d9de75fff
djh592 <dingjh21@outlook.com>
2024-10-13 22:55:27 +0800

    feat: add user logout request

--- src/components/chat/UserAvatar.tsx
+++ src/components/chat/UserAvatar.tsx
@@ -1,11 +1,17 @@
 'use client';
 import React from "react";
 import { Avatar, Box, IconButton, Menu, MenuItem, Tooltip, Typography } from "@mui/material";
-
+import { useRouter } from "next/navigation";
+import { useAppDispatch, useAppSelector } from "@/lib/hooks";
+import { setToken, setUser } from "@/lib/features/auth/authSlice";
 
 export default function UserAvatar() {
+    const router = useRouter();
+    const dispatch = useAppDispatch();
+    const token = useAppSelector((state) => state.auth.token);
+    const user = useAppSelector((state) => state.auth.user);
+
     const [anchorElUser, setAnchorElUser] = React.useState<null | HTMLElement>(null);
-    const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
 
     const handleOpenUserMenu = (event: React.MouseEvent<HTMLElement>) => {
         setAnchorElUser(event.currentTarget);
@@ -15,11 +21,43 @@ export default function UserAvatar() {
         setAnchorElUser(null);
     };
 
+    const handleLogout = async () => {
+        const headers = new Headers();
+        headers.append("Authorization", token);
+
+        const response = await fetch(`/api/logout`, {
+            method: "POST",
+            body: JSON.stringify({
+                username: user.name,
+            }),
+            headers: headers,
+        });
+
+        const data = await response.json();
+        const code = Number(data.code);
+
+        if (code === 200) {
+            dispatch(setToken(""));
+            dispatch(setUser({
+                id: "",
+                name: "",
+                password: "",
+                email: "",
+                phone: "",
+                avatar: "",
+            }));
+            router.push("/");
+        }
+        else {
+            alert("Logout failed" + data.message);
+        }
+    };
+
     return (
         <Box sx={{ flexGrow: 0 }}>
             <Tooltip title="Open settings">
                 <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                    <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
+                    <Avatar alt="Remy Sharp" src="" />
                 </IconButton>
             </Tooltip>
             <Menu
@@ -38,11 +76,20 @@ export default function UserAvatar() {
                 open={Boolean(anchorElUser)}
                 onClose={handleCloseUserMenu}
             >
-                {settings.map((setting) => (
-                    <MenuItem key={setting} onClick={handleCloseUserMenu}>
-                        <Typography sx={{ textAlign: 'center' }}>{setting}</Typography>
-                    </MenuItem>
-                ))}
+                <MenuItem key="Profile" onClick={handleCloseUserMenu}>
+                    <Typography sx={{ textAlign: 'center' }}>Profile</Typography>
+                </MenuItem>
+                <MenuItem key="Logout" onClick={() => {
+                    handleLogout().catch((err) => {
+                        alert("Logout failed: " + err);
+                    });
+                    handleCloseUserMenu();
+                }}>
+                    <Typography sx={{ textAlign: 'center' }}>Logout</Typography>
+                </MenuItem>
+                <MenuItem key="Unregister" onClick={handleCloseUserMenu}>
+                    <Typography sx={{ textAlign: 'Unregister' }}>Profile</Typography>
+                </MenuItem>
             </Menu>
         </Box>
     );
