92d3fd92b90ea09c858fa7cee0729f6d4593d0a2
djh592 <dingjh21@outlook.com>
2024-10-28 12:03:06 +0800

    feat: add basic settings page

---
+++ src/app/(dashboard)/(infopages)/settings/page.tsx
@@ -0,0 +1,14 @@
+import { Box } from "@mui/material";
+
+export default function SettingsPage() {
+    return (
+        <Box
+            sx={{
+                height: '100%',
+                width: '100%',
+            }}
+        >
+            <h1>Settings Page</h1>
+        </Box>
+    );
+}
