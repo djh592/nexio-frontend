d88450138b152da36b702702c280d85752fa034e
djh592 <dingjh21@outlook.com>
2024-10-28 12:04:40 +0800

    feat: add basic about page

---
+++ src/app/(dashboard)/(infopages)/about/page.tsx
@@ -0,0 +1,14 @@
+import { Box } from "@mui/material";
+
+export default function AboutPage() {
+    return (
+        <Box
+            sx={{
+                height: '100%',
+                width: '100%',
+            }}
+        >
+            <h1>About Page</h1>
+        </Box>
+    );
+}
