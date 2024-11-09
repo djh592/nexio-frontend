86358689090f0811040beb0161ff29fe9b27739d
djh592 <dingjh21@outlook.com>
2024-11-10 00:11:42 +0800

    fix: modify FriendPageContent

--- src/components/friend/FriendList.tsx
+++ src/components/friend/FriendList.tsx
@@ -1,22 +1,17 @@
 'use client';
 import React from 'react';
 import { useAppSelector } from '@/lib/hooks';
-import { Box, Typography, Stack } from '@mui/material';
+import { Stack } from '@mui/material';
 import UserStack from '@/components/UserStack';
 
 export default function FriendList() {
     const friends = useAppSelector((state) => state.friends.friends);
 
     return (
-        <Box sx={{ width: '100%', padding: 2 }}>
-            <Typography variant="h6" sx={{ marginBottom: 2 }}>
-                My Friends
-            </Typography>
-            <Stack spacing={2}>
-                {friends.map((friend) => (
-                    <UserStack key={friend.userId} user={friend} />
-                ))}
-            </Stack>
-        </Box>
+        <Stack spacing={2}>
+            {friends.map((friend) => (
+                <UserStack key={friend.userId} user={friend} />
+            ))}
+        </Stack>
     );
 }
--- src/components/friend/FriendPageContent.tsx
+++ src/components/friend/FriendPageContent.tsx
@@ -1,7 +1,21 @@
+import { Box, Typography, Divider } from "@mui/material";
 import FriendList from "@/components/friend/FriendList";
 
 export default function FriendPageContent() {
     return (
-        <FriendList />
+        <Box sx={{ width: '100%', padding: 2 }}>
+            <Typography
+                variant="h5"
+                sx={{
+                    fontWeight: 'bold',
+                    mb: 1,
+                    ml: 1
+                }}
+            >
+                My Friends
+            </Typography>
+            <Divider />
+            <FriendList />
+        </Box>
     );
 }
