5de92e5357bd6307f78850e341fc8c2037c10d15
djh592 <dingjh21@outlook.com>
2024-11-09 23:46:45 +0800

    fix: modify nextConfig

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: "standalone",
-    reactStrictMode: false, /* @note: To prevent duplicated call of useEffect */
-    // swcMinify: true,
     experimental: {
         ppr: 'incremental',
     },
@@ -10,7 +8,6 @@ const nextConfig = {
     async rewrites() {
         return [{
             source: "/api/:path*",
-            // TODO: Change the destination to the actual API URL
             destination: "https://nexio-backend-nexio.app.secoder.net/:path*",
         }];
     }
