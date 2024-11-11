d63972bd11cbe4b07e1e6e4469005380e905afcc
djh592 <dingjh21@outlook.com>
2024-11-11 14:28:30 +0800

    fix: modify FriendRequest definition

--- src/lib/definitions.ts
+++ src/lib/definitions.ts
@@ -11,7 +11,7 @@ export type Friends = User[];
 export type FriendRequest = {
     requestId: string;
     createdAt: string;
-    fromUser: User;
-    toUser: User;
+    fromUserId: string;
+    toUserId: string;
     status: 'pending' | 'accepted' | 'rejected';
 };
