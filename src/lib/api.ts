478926dea8bfdc16c28f1744e2b902141c9e92ba
djh592 <dingjh21@outlook.com>
2024-11-24 00:00:37 +0800

    feat: add register api

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { User } from '@/lib/definitions';
 
 const apiClient = axios.create({
     baseURL: 'https://nexio-backend-nexio.app.secoder.net',
@@ -16,4 +17,22 @@ apiClient.interceptors.request.use((config) => {
 });
 
 
+// POST: /register
+export interface RegisterRequest {
+    userName: string;
+    password: string;
+    phoneNumber?: string;
+    emailAddress?: string;
+}
 
+export interface RegisterResponse {
+    code: number;
+    info: string;
+    token: string;
+    user: User;
+}
+
+export const registerUser = async (data: RegisterRequest): Promise<RegisterResponse> => {
+    const response = await apiClient.post<RegisterResponse>('/register', data);
+    return response.data;
+};
