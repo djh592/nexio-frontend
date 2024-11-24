da0bd27abe710191ea4ddc18da4ccd8325346e4f
djh592 <dingjh21@outlook.com>
2024-11-24 14:01:04 +0800

    fix: modify definitions

--- src/lib/definitions.ts
+++ src/lib/definitions.ts
@@ -1,3 +1,7 @@
+// Definition of types and constants used in the application
+
+
+// User
 export type User = {
     id?: number;
     userId: string;
@@ -15,12 +19,14 @@ export const INITIAL_USER: User = {
     avatarUrl: "",
 };
 
-export type Friends = User[];
+export type Users = User[];
 
+
+// FriendGroup
 export type FriendGroup = {
     id?: number;
     groupName: string;
-    friends: Friends;
+    friends: Users;
 };
 
 export type FriendGroups = FriendGroup[];
@@ -34,11 +40,16 @@ export const INITIAL_FRIEND_GROUPS: FriendGroups = [
     },
 ];
 
+
+// FriendRequest
 export enum FriendRequestStatus {
+    // Only for sent requests
     Pending = 'Pending',
+    Canceled = 'Canceled',
+    // Only for received requests
     Accepted = 'Accepted',
     Rejected = 'Rejected',
-    Canceled = 'Canceled',
+    // Error status
     Failed = 'Failed'
 }
 
@@ -53,7 +64,9 @@ export type FriendRequest = {
 
 export type FriendRequests = FriendRequest[];
 
-export enum MessageType {
+
+// Message
+export enum MessageContentType {
     Text = 'Text',
     Image = 'Image',
     Video = 'Video',
@@ -62,27 +75,97 @@ export enum MessageType {
     Forwarded = 'Forwarded'
 }
 
+export type MessageContent = {
+    contentType: MessageContentType;
+    contentPayload: string;
+}
+
 export type Message = {
     messageId: string;
     createdAt: string;
     fromUserId: string;
-    type: MessageType;
-    content: string;
+    content: MessageContent;
+
+    readBy: string[]; // userIds
+    withdrawn: boolean;
+    deleted: boolean;
+
     replyMessageId?: string;
     repliedMessageId?: string;
 }
 
 export type Messages = Message[];
 
+export type MessageList = {
+    id?: number;
+    messageListId: string;
+    messages: Messages;
+};
+
+
+// ChatParticipant
+export enum ChatParticipantType {
+    Owner = 'Owner',
+    Admin = 'Admin',
+    Member = 'Member'
+};
+
+export type ChatParticipant = {
+    userId: string;
+    type: ChatParticipantType;
+};
+
+export type ChatParticipants = ChatParticipant[];
+
+export type ChatParticipantList = {
+    id?: number;
+    chatParticipantListId: string;
+    participants: ChatParticipants;
+};
+
+// ChatNotification
+export type ChatNotification = {
+    fromUserId: string;
+    createdAt: string;
+    notification: string;
+};
+
+export type ChatNotifications = ChatNotification[];
+
+export type ChatNotificationList = {
+    id?: number;
+    chatNotificationListId: string;
+    notifications: ChatNotifications;
+};
+
+// Chat
+export enum ChatType {
+    Private = 'Private',
+    Group = 'Group'
+}
+
+export type ChatSettings = {
+    isMuted: boolean;
+    isPinned: boolean;
+};
+
+export const DEFAULT_CHAT_SETTINGS: ChatSettings = {
+    isMuted: false,
+    isPinned: false,
+};
+
 export type Chat = {
     id?: number;
     chatId: string;
+    createdAt: string;
+    chatType: ChatType;
     chatName: string;
     chatAvatarUrl: string;
-    createdAt: string;
-    participants: User[];
-    messages: Messages;
+
+    messageListId: string;
+    participantListId: string;
+    notificationListId: string;
+
     unreadCount: number;
-    isMuted: boolean;
-    isPinned: boolean;
+    settings: ChatSettings;
 };
